f5bbf6b699a73f8bc32d9eda938602ff8a95078a
Joshua <joshualarido@gmail.com>
2021-09-26 16:14:10 +0700

    fixed links in html

--- resources/themes/site/default/layouts/master/layout.blade.php
+++ resources/themes/site/default/layouts/master/layout.blade.php
@@ -10,8 +10,8 @@
   <meta content="" name="keywords">
 
   <!-- Favicons -->
-  <link href="{{asset('assets/img/favicon.png')}}" rel="icon">
-  <link href="{{asset('assets/img/apple-touch-icon.png')}}" rel="apple-touch-icon">
+  <link href="{{asset('themes/default/assets/img/favicon.png')}}" rel="icon">
+  <link href="{{asset('themes/default/assets/img/apple-touch-icon.png')}}" rel="apple-touch-icon">
 
   <!-- Google Fonts -->
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Satisfy" rel="stylesheet">
@@ -45,16 +45,16 @@
   <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
   <!-- Vendor JS Files -->
-  <script src="{{asset('assets/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
-  <script src="{{asset('assets/vendor/glightbox/js/glightbox.min.js')}}"></script>
-  <script src="{{asset('assets/vendor/isotope-layout/isotope.pkgd.min.js')}}"></script>
-  <script src="{{asset('assets/vendor/php-email-form/validate.js')}}"></script>
-  <script src="{{asset('assets/vendor/purecounter/purecounter.js')}}"></script>
-  <script src="{{asset('assets/vendor/swiper/swiper-bundle.min.js')}}"></script>
-  <script src="{{asset('assets/vendor/waypoints/noframework.waypoints.js')}}"></script>
+  <script src="{{asset('themes/default/assets/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+  <script src="{{asset('themes/default/assets/vendor/glightbox/js/glightbox.min.js')}}"></script>
+  <script src="{{asset('themes/default/assets/vendor/isotope-layout/isotope.pkgd.min.js')}}"></script>
+  <script src="{{asset('themes/default/assets/vendor/php-email-form/validate.js')}}"></script>
+  <script src="{{asset('themes/default/assets/vendor/purecounter/purecounter.js')}}"></script>
+  <script src="{{asset('themes/default/assets/vendor/swiper/swiper-bundle.min.js')}}"></script>
+  <script src="{{asset('themes/default/assets/vendor/waypoints/noframework.waypoints.js')}}"></script>
 
   <!-- Template Main JS File -->
-  <script src="{{asset('assets/js/main.js')}}"></script>
+  <script src="{{asset('themes/default/assets/js/main.js')}}"></script>
 
 </body>
 
